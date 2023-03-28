0dd7f9d0939f4d548e5850f361e4c9ab12a39970
Innocent_Bern <innocentbern22@gmail.com>
2023-03-28 22:07:46 +0300

    Changes on how to handle integers

--- main.h
+++ main.h
@@ -4,10 +4,8 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 void _print_string(char *s);
-void * _print_integer(int n);
+void * _print_integer(int n, int base);
 int _isdigit(int c);
 int _str_len(char *s);
-char * _string_reverse(char * str);
-char * _int_to_string(int i, char *strout, int base);
 void _str_copy(char *dest, char *src);
 #endif
