7d935f8f0a3d98c695098208278a3fa9a1cea11d
Innocent_Bern <innocentbern22@gmail.com>
2023-03-28 21:24:51 +0300

    Handled the integer and decimal integer specifiers task 1

--- main.h
+++ main.h
@@ -7,8 +7,7 @@ void _print_string(char *s);
 void * _print_integer(int n);
 int _isdigit(int c);
 int _str_len(char *s);
-char * _itoa (int i, char *strout, int base);
-char * _strrev (char *str);
-int _atoi(char *s);
+char * _string_reverse(char * str);
+char * _int_to_string(int i, char *strout, int base);
 void _str_copy(char *dest, char *src);
 #endif
